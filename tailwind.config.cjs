df2ba037b1d8f69598cff79693373cb40845f8c9
hua-tien-thanh <huatienthanh2003@gmail.com>
2023-04-04 22:09:30 +0700

    Remove unused images Update skills Update 404 page Update About page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,15 @@ module.exports = {
       xl: '1280px',
       '2xl': '1536px',
     },
+    colors: ({ colors }) => ({
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      'accent-light': "#97DAD0",
+      'accent-regular': "#51C2B1",
+      'accent-dark': "#31746A",
+      'accent-text-over': colors.white,
+    }),
 	},
 	plugins: [],
 }
